618c090b9198cfe5d163a71e07c1f43f14a103f0
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-05-27 15:54:28 -0400

    Updates

---
+++ precache-manifest.a4ff9fd45744f8936cf39bf580b1298b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efc4f3c3112987416ee9d9b78726178",
+    "url": "/meme-maker/index.html"
+  },
+  {
+    "revision": "74a9eade803120d25436",
+    "url": "/meme-maker/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9c300368215c76ca7d00",
+    "url": "/meme-maker/static/css/main.24f966a2.chunk.css"
+  },
+  {
+    "revision": "74a9eade803120d25436",
+    "url": "/meme-maker/static/js/2.dd57cb22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-maker/static/js/2.dd57cb22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c300368215c76ca7d00",
+    "url": "/meme-maker/static/js/main.8dec62f7.chunk.js"
+  },
+  {
+    "revision": "2f90c4e3618b934f4279",
+    "url": "/meme-maker/static/js/runtime-main.a07e5cdb.js"
+  }
+]);
